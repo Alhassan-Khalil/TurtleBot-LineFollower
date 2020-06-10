a43ed88d6259764d6da0d8e15a0253cad16498cf
Alhassan Khalil <45981942+Alhassan-Khalil@users.noreply.github.com>
2020-06-10 21:09:26 +0300

    some fixes
    adding radians and some comment

--- movement_node.py
+++ movement_node.py
@@ -1,59 +1,58 @@
 #!/usr/bin/env python
+
+
 import rospy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
+from math import radians
 from std_msgs.msg import Int32
-global move_cmd
+
 
 cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 def callback(steering):
-    if steering.data == 2:
-        left_cmd = Twist()
-        left_cmd.linear.x = 0.1
-        left_cmd.angular.z = 0.1
-        cmd_vel.publish(left_cmd)
-        rospy.sleep(0.01)
-        print(steering.data)
+	# Twist is a datatype for velocity
+	forword_cmd = Twist()
+	# let's go forward at 0.2 m/s
+	forword_cmd.linear.x = 0.2
+	
+	left_cmd = Twist()
+	left_cmd.linear.x = 0.1
+	left_cmd.angular.z = radians(45); #45 deg/s in radians/s
 
-    elif steering.data == 3:
-        right_cmd = Twist()
-        right_cmd.linear.x = 0.1
-        right_cmd.angular.z = -0.1
-        cmd_vel.publish(right_cmd)
-        rospy.sleep(0.01)
-        print(steering.data)
-    elif steering.data == 1:
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.15
-        move_cmd.angular.z= 0
-        cmd_vel.publish(move_cmd)
-        rospy.sleep(0.01)
-        print(steering.data)
-    elif steering.data == 0:
-        search_cmd = Twist()
-        search_cmd.linear.x = 0.0
-        search_cmd.angular.z = 0.25
-        cmd_vel.publish(search_cmd)
-        rospy.sleep(0.01)
-        print(steering.data)
+	right_cmd = Twist()
+	right_cmd.linear.x = 0.1
+	right_cmd.angular.z = radians(-45); #45 deg/s in radians/s
+
+	Search_cmd = Twist()
+	Search_cmd.linear.x = 0
+	Search_cmd.angular.z = radians(45); #45 deg/s in radians/s
+#-----------------------------------------------------------------------------
+
+	if steering.data == 1:
+		# publish the velocity
+		cmd_vel.publish(forword_cmd)
+		rospy.sleep(0.01)
+		rospy.loginfo("Straight")
+	elif steering.data== 2:
+		cmd_vel.publish(left_cmd)
+		rospy.sleep(0.01)
+		rospy.loginfo("Left")
+	elif steering.data== 3:
+		cmd_vel.publish(right_cmd)
+		rospy.sleep(0.01)
+		rospy.loginfo("right")
+	elif steering.data== 0:
+		cmd_vel.publish(Search_cmd)
+		rospy.sleep(0.01)
+		rospy.loginfo("search")
 
 
 def movement():
-    rospy.init_node('movement', anonymous=True)
-    r = rospy.Rate(10) #10hz
-    rospy.Subscriber('/vision_node', Int32, callback)
-    rospy.spin()
-    #move_cmd = Twist()
-    #move_cmd.linear.x = 0
-    #move_cmd.angular.z = 2
-
-    #while not rospy.is_shutdown():
-        #cmd_vel.publish(move_cmd)
-        #callback()
-        #r.sleep()
+	rospy.init_node('movement', anonymous=True)
+	steering = rospy.Subscriber("/vision_node",Int32,callback)
+	rospy.spin()
 
 
 if __name__ == '__main__':
-    try:
-        movement()
-    except rospy.ROSInterruptException: pass
+	try:
+		movement()
+	except rospy.ROSInterruptException: pass
